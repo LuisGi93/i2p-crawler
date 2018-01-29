388aa140a6da54205b69583bdb8459ef06a23958
LuisGi93 <luisgguijarro9@gmail.com>
2018-01-29 18:52:59 +0100

    Modificamos la arania para evitar que solicite recursos multimedia y scripts, aniadimos filtro para evitar el parseo de paginas que no sean de texto

--- i2p_crawler/i2p_crawler/spiders/i2p_spiders.py
+++ i2p_crawler/i2p_crawler/spiders/i2p_spiders.py
@@ -9,42 +9,67 @@ import logging
 class QuotesSpider1(scrapy.Spider):
 
     name = "i2p_spider_1"
-    extractor= LinkExtractor(tags=('a','area','link','img','script'),attrs=('src','href'), deny_extensions=())
+    extractor_links= LinkExtractor(tags=('a','area','link'),attrs=('src','href'), deny_extensions=())
+    extractor_resources= LinkExtractor(tags=('img','script','audio'),attrs=('src','href'), deny_extensions=())
+
     visited_links=[]
 
     def __init__(self, url_to_crawl=None):
-        extracted_urls=[]
+        self.extracted_urls=[]
         #Obtenemos el dominio de la url
-        url=url_to_crawl
-        source_domain= "{0.scheme}://{0.netloc}/".format(urlparse.urlsplit(url))
+        self.start_urls  = [url_to_crawl] #A;adir mas semillas
+        self.source_domain= "{0.scheme}://{0.netloc}/".format(urlparse.urlsplit(url_to_crawl))
+        self.white_list_types=["text/plain", "text/html","text/xml"]
 
+    def closed(self, reason):
+        self.logger.error("Links extraidos=[%s]", json.dumps(self.extracted_urls))
+        fh = open("spider1.txt","w")
+        fh.write(json.dumps(self.extracted_urls))
+        fh.close()
+
+
+    #Las unicas urls que entran aqui son las que queremos ver si contienen mas urls
     def extract_links(self,response):
-        links=self.extractor.extract_links(response)
-        for link in links:
+        
+        self.logger.error("Headers %s", response.headers)
+        url_type=response.headers['Content-Type']
+        self.logger.error("Headers %s", url_type)
+        safe_to_scrape=False
+        for safe_type in self.white_list_types:
+            if safe_type in url_type:
+                safe_to_scrape=True
+        if safe_to_scrape:
+            links_to_visit=self.extractor_links.extract_links(response)
+            request_list=[]
             D={}
-            destination_domain= "{0.scheme}://{0.netloc}/".format(urlparse.urlsplit(link.url)) 
-            link_cleaned="{0.scheme}://{0.netloc}{0.path}".format(urlparse.urlsplit(link.url))
+            D[response.request.url]=[]
+            for link in links_to_visit:
+                destination_domain= "{0.scheme}://{0.netloc}/".format(urlparse.urlsplit(link.url)) 
+                link_cleaned="{0.scheme}://{0.netloc}{0.path}".format(urlparse.urlsplit(link.url))
+                if self.source_domain == destination_domain and link_cleaned not in self.visited_links:  
+                    self.visited_links.append(link_cleaned)
+                    request=scrapy.Request(link_cleaned, callback=self.parse,errback = self.err, dont_filter=True)
+                    request_list.append(request)
+                elif self.source_domain != destination_domain:
+                    D[response.request.url].append(link_cleaned)
 
-            if source_domain == destination_domain and link_cleaned not in self.visited_links:  
-                visited_links.append(link_cleaned)
-                request=scrapy.Request(link_cleaned, callback=self.parse,errback = self.err, dont_filter=True)
-                request_list.append(request)
-            elif source_domain != destination_domain:
-                D[response.request.url].append(link_cleaned)
-        extracted_urls.append(D)
-        return request_list
+            links_not_visit=self.extractor_resources.extract_links(response)
+            for link_resources in links_not_visit:
+                destination_domain= "{0.scheme}://{0.netloc}/".format(urlparse.urlsplit(link_resources.url)) 
+                link_cleaned="{0.scheme}://{0.netloc}{0.path}".format(urlparse.urlsplit(link_resources.url))
+                if self.source_domain != destination_domain:
+                    D[response.request.url].append(link_cleaned)
+
+            self.extracted_urls.append(D)
+            return request_list
 
     #Reporta los errores que se producen en caso de que una peticion falle
     def err(self,failure):
         self.logger.error(repr(failure))
 
     def parse(self, response):
-        request_list=extract_links(response)
-        if len(request_list) == 0:
-            #TODO: Pasarle todos los links al padre
-            self.logger.warning('Links extraidos %s', json.dumps(extracted_urls))
-            
-        else:
+        request_list=self.extract_links(response)
+        if request_list:
             for request in request_list:
-                    yield request
+                yield request
 
